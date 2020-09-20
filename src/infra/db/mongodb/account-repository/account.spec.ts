b4a26b3f07523ae74589f7ddf2791e5e045cb831
Wellington Perez <wellingtonaperez@gmail.com>
2020-09-20 19:20:59 -0300

    test: ensure AccountMogoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,23 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
+
+describe('Accout Mongo Repository', () => {
+  beforeAll(async () => await MongoHelper.connect(process.env.MONGO_URL))
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+  test('should return an account success', async () => {
+    const sut = new AccountMongoRepository()
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any@email.com',
+      password: 'any_password'
+    })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any@email.com')
+    expect(account.password).toBe('any_password')
+  })
+})
