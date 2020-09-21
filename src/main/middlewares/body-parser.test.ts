fd4565425c4e96f0556d071c834643deb191a50c
Wellington Perez <wellingtonaperez@gmail.com>
2020-09-21 13:26:00 -0300

    feat: ensure cors is enabled

--- src/main/middlewares/body-parser.test.ts
+++ src/main/middlewares/body-parser.test.ts
@@ -2,13 +2,14 @@ import request from 'supertest'
 import app from '../config/app'
 
 describe('Body Parser Middleware', () => {
+  const uri = '/test_body_parser'
   test('should parse body as json', async () => {
-    app.post('/test_body_parser', (req, res) => {
+    app.post(uri, (req, res) => {
       res.send(req.body)
     })
     await request(app)
-      .post('/test_body_parser')
-      .send({ name: 'Rodrigo' })
-      .expect({ name: 'Rodrigo' })
+      .post(uri)
+      .send({ name: 'Wellington' })
+      .expect({ name: 'Wellington' })
   })
 })
