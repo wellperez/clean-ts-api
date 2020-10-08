6979b0678f85e8c31c168f3f6920cd5786b158fb
Wellington Perez <wellingtonaperez@gmail.com>
2020-10-08 14:17:37 -0300

    feat: add LoginValidationFactory

--- src/main/factories/signup/signup.ts
+++ src/main/factories/signup/signup.ts
@@ -1,10 +1,10 @@
-import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
-import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
-import { LogMongoRepository } from '../../infra/db/log-repository/log'
-import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
-import { SignUpController } from '../../presentation/controllers/signup/signup'
-import { Controller } from '../../presentation/protocols'
-import { LogControllerDecorator } from '../decorators/log'
+import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
+import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter'
+import { LogMongoRepository } from '../../../infra/db/log-repository/log'
+import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account'
+import { SignUpController } from '../../../presentation/controllers/signup/signup'
+import { Controller } from '../../../presentation/protocols'
+import { LogControllerDecorator } from '../../decorators/log'
 import { makeSignUpValidation } from './signup-validation'
 
 export const makeSignUpController = (): Controller => {
