227828b2603ca9641a46647b4c8c5bbc812f4f68
Wellington Perez <wellingtonaperez@gmail.com>
2020-10-21 13:59:50 -0300

    refactor: remove duplicated code

--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -3,13 +3,10 @@ import { Validation } from '../../protocols/validation'
 import { HttpRequest, HttpResponse, Controller, AddAccount } from './signup-controller-protocols'
 
 export class SignUpController implements Controller {
-  private readonly addAccount: AddAccount
-  private readonly validation: Validation
-
-  constructor (addAccount: AddAccount, validation: Validation) {
-    this.addAccount = addAccount
-    this.validation = validation
-  }
+  constructor (
+    private readonly addAccount: AddAccount,
+    private readonly validation: Validation
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
