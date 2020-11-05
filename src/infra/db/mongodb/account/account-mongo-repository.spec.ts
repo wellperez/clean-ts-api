76292752b4fab338647e6a541dad4f57b28a0d0a
Wellington Perez <wellingtonaperez@gmail.com>
2020-11-05 09:24:41 -0300

    test: ensure AccountMongoRepository find an account by token with role

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -92,5 +92,22 @@ describe('Accout Mongo Repository', () => {
       expect(account.email).toBe('any@email.com')
       expect(account.password).toBe('any_password')
     })
+
+    test('should return an account on loadByToken success with role', async () => {
+      const sut = makeSut()
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any@email.com',
+        password: 'any_password',
+        accessToken: 'any_token',
+        role: 'any_role'
+      })
+      const account = await sut.loadByToken('any_token', 'any_role')
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any@email.com')
+      expect(account.password).toBe('any_password')
+    })
   })
 })
