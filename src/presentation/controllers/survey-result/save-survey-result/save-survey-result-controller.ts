3ab58c75b0eb060827954d80b4ed6295189ec493
Wellington Perez <wellingtonaperez@gmail.com>
2020-12-14 14:18:13 -0300

    feat: ensure SaveSurveyResultController returns 200 on success

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -5,6 +5,7 @@ import {
   HttpResponse,
   InvalidParamError,
   LoadSurveyById,
+  ok,
   SaveSurveyResult,
   serverError
 } from './save-survey-result-controller-protocols'
@@ -33,13 +34,13 @@ export class SaveSurveyResultController implements Controller {
       } else {
         return forbidden(new InvalidParamError('surveyId'))
       }
-      await this.saveSurveyResult.save({
+      const surveyResult = await this.saveSurveyResult.save({
         accountId,
         surveyId,
         answer,
         date: new Date()
       })
-      return null
+      return ok(surveyResult)
     } catch (err) {
       return serverError(err)
     }
