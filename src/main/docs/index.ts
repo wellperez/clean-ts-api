6acb855365f4ae2406335e570fa184bb040df8f0
Wellington Perez <wellingtonaperez@gmail.com>
2020-12-28 13:41:11 -0300

    feat: create api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,13 @@
+import paths from './paths'
+import components from './components'
+import schemas from './schemas'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: '4Dev - Enquetes para Programadores',
-    description: 'Essa é a documentação da API feita pelo aluno Wellington Perez no curso da Udemy de NodeJs usando Typescript, TDD, Clean Architecture e seguindo os princípios do SOLID e Design Patterns.',
+    description:
+      'Essa é a documentação da API feita pelo aluno Wellington Perez no curso da Udemy de NodeJs usando Typescript, TDD, Clean Architecture e seguindo os princípios do SOLID e Design Patterns.',
     version: '1.0.0',
     contact: {
       name: 'Wellington Perez',
@@ -16,17 +21,26 @@ export default {
   },
   externalDocs: {
     description: 'Link para o treinamento completo',
-    url: 'https://www.udemy.com/course/tdd-com-mango/?referralCode=B53CE5CA2B9AFA5A6FA1'
+    url:
+      'https://www.udemy.com/course/tdd-com-mango/?referralCode=B53CE5CA2B9AFA5A6FA1'
   },
-  servers: [{
-    url: '/api',
-    description: 'Servidor Principal'
-  }],
-  tags: [{
-    name: 'Login',
-    description: 'APIs relacionadas a Login'
-  }, {
-    name: 'Enquete',
-    description: 'APIs relacionadas a Enquete'
-  }]
+  servers: [
+    {
+      url: '/api',
+      description: 'Servidor Principal'
+    }
+  ],
+  tags: [
+    {
+      name: 'Login',
+      description: 'APIs relacionadas a Login'
+    },
+    {
+      name: 'Enquete',
+      description: 'APIs relacionadas a Enquete'
+    }
+  ],
+  paths,
+  schemas,
+  components
 }
